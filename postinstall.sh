0eaaf43b0f46dee69cb710be63bfb963a542f8f4
Rockheung <rockheung@gmail.com>
2020-04-13 13:09:16 +0900

    Update: 스크립트 개선

--- postinstall.sh
+++ postinstall.sh
@@ -6,13 +6,13 @@ fi
 
 if [ -f "employees_db/employees.sql_" ]; then
   rm -rf employees_db
-  tar -xvjf employees_db-full.tar.bz2
 fi
 
-# FK가 잘 걸려있는 경우
-sed -i_ "s/storage_engine/default_storage_engine/g; s/load_/employees_db\/load_/g; /^\ *FOREIGN/d; /^\ *KEY/d; s/employees;/$SRC_DB_NAME;/g" employees_db/employees.sql
+tar -xvjf employees_db-full.tar.bz2
+
 
 # FK 없이 애플리케이션 수준에서 관계가 걸려있는 경우
-# sed -i_ "s/storage_engine/default_storage_engine/g; s/load_/employees_db\/load_/g; s/employees;/$SRC_DB_NAME;/g" employees_db/employees.sql
+sed -i_ "s/storage_engine/default_storage_engine/g; s/load_/employees_db\/load_/g; /^\ *FOREIGN/d; /^\ *KEY/d; s/employees;/$SRC_DB_NAME;/g" employees_db/employees.sql
 
-cp .env.example .env
+# FK가 잘 걸려있는 경우
+# sed -i_ "s/storage_engine/default_storage_engine/g; s/load_/employees_db\/load_/g; s/employees;/$SRC_DB_NAME;/g" employees_db/employees.sql
